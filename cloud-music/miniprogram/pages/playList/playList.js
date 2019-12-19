06f14df25318227512bcc00dd02c08adfc18efdf
liyanjun <254948133@qq.com>
2019-12-19 17:14:42 +0800

    初始化

---
+++ cloud-music/miniprogram/pages/playList/playList.js
@@ -0,0 +1,62 @@
+// pages/playList/playList.js
+const db = wx.cloud.database({
+  env: "cloud-fun-1a4ff0"
+});
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imageList: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this._getSwiper();
+   
+  },
+  _getSwiper() {
+    db.collection('swiper').get().then((res) => {
+      this.setData({
+        imageList: res.data
+      })
+    })
+    db.collection('playList').get().then((res) => {
+     console.log(res, 1111)
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    wx.cloud.callFunction({
+      name: 'playList'
+    }).then((res) => {
+      console.log(res)
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  }
+})
