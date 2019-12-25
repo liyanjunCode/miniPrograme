9a4196c548cf512e51e3acbebc4258219f782069
liyanjun <254948133@qq.com>
2019-12-25 18:46:40 +0800

    小程序首页列表

--- cloud-music/cloudfunctions/getPlaylist/index.js
+++ cloud-music/cloudfunctions/getPlaylist/index.js
@@ -12,18 +12,53 @@ exports.main = async (event, context) => {
   const countResult = await playlistCollection.count()
   const count = countResult.total
   const PageNum = Math.ceil(count / 100)
-  const taskList = []
+  let taskList = []
+  // 因为云数据库只能每次查询100条， 所以需算出按100条查，需查询几次
   for (let i = 0; i < PageNum; i++) {
-    playlistCollection.skip(i * MAX_LImIT).limit(MAX_LImIT).get()
+    let promise = playlistCollection.skip(i * MAX_LImIT).limit(MAX_LImIT).get()
+    taskList.push(promise)
   }
-  console.log(PageNum)
+  let list = {
+    data: []
+  }
+  // 获取到数据库中所有的歌曲列表
+  if (taskList.length > 0) {
+    list = (await Promise.all(taskList)).reduce((prev, cur) => {
+      return {
+        data: acc.data.concat(cur.data)
+      }
+    })
+  }
+  // 从接口获取歌曲推荐列表
   const playlist = await rp(URL).then((res) => {
     return JSON.parse(res).result
   })
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+  const databaseList = []
+  // 循环数据库和接口的歌曲对比去重
+  for (let i = 0, l = playlist.length; i<l;i++) {
+    let flag = true
+    for (let j = 0, len = list.data.length; j < len; j++) {
+      if (playlist[i].id === list.data[j].id) {
+        flag = false
+        break
+      }
+    }
+    if (flag) {
+      databaseList.push(playlist[i])
+    }
   }
+  // 将数据库不存在的歌曲存入数据库
+  for (let i = 0, l = databaseList.length; i < l; i++) {
+    playlistCollection.add({
+      data:{
+        ...databaseList[i],
+        createTime: db.serverDate()
+      }
+    }).then((res) => {
+      console.log('插入成功')
+    }).catch((err) => {
+      console.log('插入失败')
+    })
+  }
+  return databaseList.length
 }
--- cloud-music/miniprogram/pages/musiclist/musiclist.js
+++ cloud-music/miniprogram/pages/musiclist/musiclist.js
@@ -1,43 +1,25 @@
-// pages/playList/playList.js
-const db = wx.cloud.database({
-  env: "cloud-fun-1a4ff0"
-});
+// miniprogram/pages/musiclist/musiclist.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    imageList: [],
-    playlist: []
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this._getSwiper();
-    this._getPlayList()
+
   },
-  _getSwiper() {
-    db.collection('swiper').get().then((res) => {
-      this.setData({
-        imageList: res.data
-      })
-    })
-  },
-  _getPlayList() {
-    db.collection('playlist').get().then((res) => {
-      this.setData({
-        playlist: res.data
-      })
-    })
-  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
@@ -60,7 +42,25 @@ Page({
   onUnload: function () {
 
   },
-  onReachBottom(res) {
-    console.log(111)
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
